095a026343eb4e26476b2800dc53578393527ed0
Finn <finn@uwave.fm>
2016-05-08 00:32:03 -0700

    Correct path in app_menu.php

--- app_menu.php
+++ app_menu.php
@@ -4,5 +4,5 @@ $apps[$x]['menu'][$y]['title']['en-us'] = "Call Screening";
 $apps[$x]['menu'][$y]['uuid'] = "27a8193d-f6c5-4d3e-9b0f-3c32316fc1a9";
 $apps[$x]['menu'][0]['parent_uuid'] = "fd29e39c-c936-f5fc-8e2b-611681b266b5";
 $apps[$x]['menu'][$y]['category'] = "internal";
-$apps[$x]['menu'][$y]['path'] = "/app/contact-importer/";
+$apps[$x]['menu'][$y]['path'] = "/app/call_screening/";
 $apps[$x]['menu'][$y]['groups'][] = "superadmin";
